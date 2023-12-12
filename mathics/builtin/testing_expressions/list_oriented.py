8bb1d276a8387d18cbec3ae809cdd29ad0046da4
Li-Xiang-Ideal <54926635+Li-Xiang-Ideal@users.noreply.github.com>
2023-12-12 21:53:14 +0800

    Fix_ArrayQ

--- mathics/builtin/testing_expressions/list_oriented.py
+++ mathics/builtin/testing_expressions/list_oriented.py
@@ -39,14 +39,14 @@ class ArrayQ(Builtin):
      = False
     >> ArrayQ[{{a, b}, {c, d}}, 2, SymbolQ]
      = True
+     
+    ArrayQ works on sparse arrays:
     >> ArrayQ[SparseArray[{{1, 2} -> a, {2, 1} -> b}]]
      = True
-    >> ArrayQ[SparseArray[{{1, 2} -> a, {2, 1} -> b}], 1]
-     = False
+
+    For sparse arrays, ArrayQ tests not only specified values, but also default value 0:
     >> ArrayQ[SparseArray[{{1, 2} -> a, {2, 1} -> b}], 2, SymbolQ]
      = False
-    >> ArrayQ[SparseArray[{{1, 1} -> a, {1, 2} -> b}], 2, SymbolQ]
-     = True
     """
 
     rules = {
