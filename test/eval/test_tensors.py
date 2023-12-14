b3c900411c3dc4658f0485a182b31c113e9654f4
Li-Xiang-Ideal <54926635+Li-Xiang-Ideal@users.noreply.github.com>
2023-12-14 15:35:52 +0800

    Add unit test for eval.tensors

---
+++ test/eval/test_tensors.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Unit tests for mathics.eval.tensors
+"""
+import unittest
+
+from mathics.eval.tensors import unpack_outer
+
+
+class TestTensors(unittest.TestCase):
+    def test_unpack_outer(self):
+        assert True
+
+
+if __name__ == "__main__":
+    unittest.main()
